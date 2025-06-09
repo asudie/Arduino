b0fdb4be9f8f6357cc03b2e2b1e31ddf9b4dad1d
Aleksandra Smolniakova <smolnyakovaae@gmail.com>
2025-06-09 17:32:58 +0200

    Update ex00.ino

--- ex00.ino
+++ ex00.ino
@@ -1,67 +1,82 @@
-int red = 13;
-int yellow = 12;
-int green = 8;
+int ledOne = 13;
+int ledTwo = 12;
+int ledThree = 8;
 int buttonPin = 2;
 
-bool ledMode = false;
-bool lastButtonState = HIGH;
+bool ledMode = false;             
+bool lastButtonState = HIGH;      
+bool currentButtonState;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+unsigned long lastBlinkTime = 0;
+unsigned long blinkInterval = 1000;  // 1 second
+bool isBlinkOn = false;
+
+#define MOD 2
 
 void setup()
 {
-  pinMode(red, OUTPUT);
-  pinMode(yellow, OUTPUT);
-  pinMode(green, OUTPUT);
+  pinMode(ledOne, OUTPUT);
+  pinMode(ledTwo, OUTPUT);
+  pinMode(ledThree, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
-void loop()
-{
-  bool currentButtonState = digitalRead(buttonPin);
-  
-  if (lastButtonState == HIGH && currentButtonState == LOW) {
-    ledMode = !ledMode;
-    Serial.print("TOGGLED! Mode is now: ");
-    Serial.println(ledMode ? "ON" : "OFF");
-    delay(50);
+void checkState() {
+  bool reading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
   }
 
-  lastButtonState = currentButtonState;
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != currentButtonState) {
+      currentButtonState = reading;
+      if (currentButtonState == LOW) {
+        ledMode = !ledMode;
+        Serial.print("TOGGLED! Mode is now: ");
+        Serial.println(ledMode ? "ON" : "OFF");
+      }
+    }
+  }
+
+  lastButtonState = reading;
+}
+
+void loop()
+{
+  checkState();
 
   if (!ledMode) {
-    digitalWrite(green, 1);
-    digitalWrite(red, 0);
-    digitalWrite(yellow, 0);
-  } else {
-    // mandatory
-    digitalWrite(green, 0);
-    digitalWrite(red, 1);
-    digitalWrite(yellow, 1);
-    delay(1000);
-    digitalWrite(red, 0);
-    digitalWrite(yellow, 0);
-    delay(1000);
+    digitalWrite(ledThree, HIGH);
+    digitalWrite(ledOne, LOW);
+    digitalWrite(ledTwo, LOW);
+    isBlinkOn = false;  // reset blinking state
+  } else if(MOD == 1){
+    digitalWrite(ledThree, LOW);
 
-    // bonus 1
-    /*digitalWrite(green, 0);
-    digitalWrite(red, 1);
-    delay(1000);
-    digitalWrite(red, 0);
-    digitalWrite(yellow, 1);
-    delay(1000);
-    digitalWrite(yellow, 0);*/
+    unsigned long currentMillis = millis();
+    if (currentMillis - lastBlinkTime >= blinkInterval) {
+      lastBlinkTime = currentMillis;
+      isBlinkOn = !isBlinkOn;
+    }
 
-    // bonus 2
-    /*digitalWrite(green, 0);
-    digitalWrite(red, 1);
-    digitalWrite(yellow, 1);
-    delay(500);
-    digitalWrite(red, 0);
-    delay(500);
-    digitalWrite(red, 1);
-    digitalWrite(yellow, 0);
-    delay(500);
-    digitalWrite(red, 0);
-    delay(500);*/
+    digitalWrite(ledOne, isBlinkOn ? HIGH : LOW);
+    digitalWrite(ledTwo, isBlinkOn ? HIGH : LOW);
+  } else if(MOD == 2)
+  {
+    digitalWrite(ledThree, LOW);
+
+    unsigned long currentMillis = millis();
+    if (currentMillis - lastBlinkTime >= blinkInterval) {
+      lastBlinkTime = currentMillis;
+      isBlinkOn = !isBlinkOn;
+    }
+
+    digitalWrite(ledOne, isBlinkOn ? HIGH : LOW);
+    digitalWrite(ledTwo, isBlinkOn ? LOW : HIGH);
   }
 }
